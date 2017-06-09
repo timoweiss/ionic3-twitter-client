37ea2a7dd089661869884d2e8e913cdb4a70d62e
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-09 10:47:39 +0200

    cleanup, refactoring

--- src/components/TweetList/TweetList.component.ts
+++ src/components/TweetList/TweetList.component.ts
@@ -9,29 +9,23 @@ import { IStatus } from '../../services/twitter.service';
 })
 export class TweetList implements OnInit, OnChanges {
 
-    @Input() name: string;
-
+    // actually rendered tweets
     private _tweets: Array<IStatus> = [];
+
     @Input()
     private tweets: Array<IStatus> = [];
 
+    @Input()
+    private clearall: boolean;
+
+    // buffered tweets, they are loaded but not shown
     private tweetBuffer: Array<IStatus> = [];
 
+    // show loading-indicator
     isLoading: boolean = false;
 
-    // @Input()
-    // set tweets(tweets: Array<IStatus>) {
-    //     debugger
-    //     if (!tweets) {
-    //         // probably loading?
-    //         return;
-    //     }
-    //     this.loader ? this.loader.dismiss() : null;
-    //     this._tweets = tweets;
-    // }
-
     constructor(public navCtrl: NavController) {
-        // setInterval(() => console.log(this.tweets), 2000)
+
     }
 
     flushBuffer() {
@@ -46,27 +40,44 @@ export class TweetList implements OnInit, OnChanges {
 
 
     ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
+
         this.isLoading = false;
-        if (changes.tweets.firstChange) {
+        console.log({ clearall: changes.clearall })
 
-        } else {
-            if (!this._tweets.length) {
-                return this._tweets = changes.tweets.currentValue;
-            }
-
-            console.log('new tweets available');
-            // are there any new tweets which are not in the shown list?
-            const newTweetsList = this.getOutersection(changes.tweets.currentValue, this._tweets)
-            // are there any new tweets which are not in the shown list and not in buffer?
-            const newTweetsBuffer = this.getOutersection(newTweetsList, this.tweetBuffer)
-
-            this.tweetBuffer = [...newTweetsBuffer, ...this.tweetBuffer]
+        if (changes.clearall && !changes.clearall.firstChange) {
+            this.handleClearallChange(changes.clearall);
         }
-        console.log(changes)
+
+        if (changes.tweets && !changes.tweets.firstChange) {
+            this.handleTweetsChange(changes.tweets);
+        }
     }
 
     private getOutersection(newList, oldList) {
         return newList.filter(tweet => !oldList.some(existingTweet => existingTweet.id === tweet.id))
+    }
+
+    private handleTweetsChange(tweetChange) {
+        if (!this._tweets.length) {
+            return this._tweets = tweetChange.currentValue;
+        }
+        // are there any new tweets which are not in the shown list?
+        const newTweetsList = this.getOutersection(tweetChange.currentValue, this._tweets)
+        // are there any new tweets which are not in the shown list and not in buffer?
+        const newTweetsBuffer = this.getOutersection(newTweetsList, this.tweetBuffer)
+
+        this.tweetBuffer = [...newTweetsBuffer, ...this.tweetBuffer]
+    }
+
+    private handleClearallChange(clearallChange) {
+        if (clearallChange.currentValue) {
+            this.clearAllTweets();
+        }
+    }
+
+    clearAllTweets() {
+        this._tweets = [];
+        this.tweetBuffer = [];
     }
 
 }
