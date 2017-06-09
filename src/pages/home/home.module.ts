e81e56139a7a0f9175d36319ee0fbe87687ade57
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-09 16:01:58 +0200

    add search pipe to declarations

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { HomePage } from './home';
 import { IonicPageModule } from 'ionic-angular';
 import { TweetList } from '../../components/TweetList/TweetList.component';
+import { SearchPipe } from '../../pipes/search/search';
 
 @NgModule({
-    declarations: [HomePage, TweetList],
+    declarations: [HomePage, TweetList, SearchPipe],
     imports: [IonicPageModule.forChild(HomePage)],
 })
 export class HomePageModule { }
