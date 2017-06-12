1e16f6d7461b64789c92e12accd29275f6b12b72
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-12 17:12:32 +0200

    add reducer

---
+++ src/reducers/tweetsReducer.ts
@@ -0,0 +1,64 @@
+import { ActionReducer, Action } from '@ngrx/store';
+
+import { ISearchMetadata } from '../services/twitter.service';
+
+export const FETCH_SEARCH_TWITTER = 'FETCH_SEARCH_TWITTER';
+export const RECEIVE_SEARCH_TWITTER = 'RECEIVE_SEARCH_TWITTER';
+export const FETCH_NEXT_TWEETS = 'FETCH_NEXT_TWEETS';
+export const RECEIVE_NEXT_TWEETS = 'RECEIVE_NEXT_TWEETS';
+export const RESET_TWEETS = 'RESET_TWEETS';
+
+export interface TweetState {
+    tweets: Array<any>
+    tweetsMeta: ISearchMetadata
+    searchTerm: string
+    receivedAt: number
+    isFetching: boolean
+}
+
+const initialState: TweetState = {
+    tweets: [],
+    tweetsMeta: null,
+    searchTerm: '#javascript',
+    receivedAt: 0,
+    isFetching: false,
+}
+
+export function tweetsReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case FETCH_SEARCH_TWITTER:
+            return { ...state, isFetching: true };
+        case RECEIVE_SEARCH_TWITTER: {
+            return {
+                ...state,
+                tweets: action.payload.tweets,
+                tweetsMeta: action.payload.tweetsMeta,
+                searchTerm: decodeURIComponent(action.payload.tweetsMeta.query),
+                isFetching: false,
+            };
+        }
+
+        case FETCH_NEXT_TWEETS: {
+            return {
+                ...state,
+                isFetching: true,
+            }
+        }
+
+        case RECEIVE_NEXT_TWEETS: {
+            debugger
+            return {
+                ...state,
+                tweets: [...state.tweets, ...action.payload.tweets],
+                isFetching: false,
+            }
+        }
+
+
+        case RESET_TWEETS:
+            return { ...state, ...initialState };
+
+        default:
+            return state;
+    }
+}
