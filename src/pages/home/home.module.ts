f40d1b34e0f081952e488a703e3834dc9c85cda8
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-13 16:25:03 +0200

    add SpeechRecognition provider

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomePage } from './home';
 import { IonicPageModule } from 'ionic-angular';
 import { TweetList } from '../../components/TweetList/TweetList.component';
 import { SearchPipe } from '../../pipes/search/search';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 @NgModule({
     declarations: [HomePage, TweetList, SearchPipe],
     imports: [IonicPageModule.forChild(HomePage)],
+    providers: [SpeechRecognition]
 })
 export class HomePageModule { }
