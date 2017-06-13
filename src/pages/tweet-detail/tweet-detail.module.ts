514e674d599e8a88144720e15b0925f4c9d6b8c0
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-13 11:54:25 +0200

    if tweet is retweet -> show original, add angular-linky

--- src/pages/tweet-detail/tweet-detail.module.ts
+++ src/pages/tweet-detail/tweet-detail.module.ts
@@ -2,9 +2,13 @@
 import { NgModule } from '@angular/core';
 import { TweetDetailPage } from './tweet-detail';
 import { IonicPageModule } from 'ionic-angular';
+import { LinkyModule } from 'angular-linky';
 
 @NgModule({
     declarations: [TweetDetailPage],
-    imports: [IonicPageModule.forChild(TweetDetailPage)],
+    imports: [
+        IonicPageModule.forChild(TweetDetailPage),
+        LinkyModule,
+    ],
 })
 export class HomePageModule { }
--- src/pages/tweet-detail/tweet-detail.ts
+++ src/pages/tweet-detail/tweet-detail.ts
@@ -15,35 +15,42 @@ import { IStatus } from '../../services/twitter.service';
         </ion-header>
         <ion-content>
             <ion-card>
-            <ion-item>
-                <ion-avatar item-start>
-                <img src="{{selectedTweet.user.profile_image_url_https}}">
-                </ion-avatar>
-                <h2>{{selectedTweet.user.name}}</h2>
-                <p>@{{selectedTweet.user.screen_name}}</p>
-            </ion-item>
+                
+                <ion-item>
+                    <ion-row *ngIf="isRetweet">
+                        <ion-note item-end style="text-align:right;">
+                            @{{parentTweet.user.screen_name}} has retweeted
+                        </ion-note>
+                    </ion-row>
+                    <ion-avatar [ngStyle]="{'padding-top': isRetweet ? '30px' : 'inherit'}" item-start>
+                        <img src="{{selectedTweet.user.profile_image_url_https}}">
+                    </ion-avatar>
+                    
+                    <h2>{{selectedTweet.user.name}}</h2>
+                    <p>@{{selectedTweet.user.screen_name}}</p>
+                </ion-item>
 
                 <ion-card-content>
-                    {{selectedTweet.text}}
+                    <span [innerHTML]="selectedTweet.text | linky:{mention:'twitter', hashtag:'twitter' }"></span>
                 </ion-card-content>
 
                 <ion-row>
                     <ion-col>
-                    <button ion-button icon-left clear small>
-                        <ion-icon name="star"></ion-icon>
-                        <div>12 Likes</div>
-                    </button>
+                        <button ion-button icon-left clear small>
+                            <ion-icon name="heart"></ion-icon>
+                            <div>{{selectedTweet.favorite_count || 0}}</div>
+                        </button>
                     </ion-col>
                     <ion-col>
-                    <button ion-button icon-left clear small>
-                        <ion-icon name="repeat"></ion-icon>
-                        <div>4 Comments</div>
-                    </button>
+                        <button ion-button icon-left clear small>
+                            <ion-icon name="repeat"></ion-icon>
+                            <div>{{selectedTweet.retweet_count || 0}}</div>
+                        </button>
                     </ion-col>
                     <ion-col center text-center>
-                    <ion-note>
-                        {{selectedTweet.created_at | date}}
-                    </ion-note>
+                        <ion-note item-end>
+                            {{selectedTweet.created_at | date}}
+                        </ion-note>
                     </ion-col>
                 </ion-row>
             </ion-card>
@@ -53,9 +60,18 @@ import { IStatus } from '../../services/twitter.service';
 export class TweetDetailPage {
 
     private selectedTweet: IStatus = null;
+    private parentTweet: IStatus = null;
+    private isRetweet: boolean;
 
     constructor(public navParams: NavParams) {
-        this.selectedTweet = navParams.data;
+        this.selectedTweet = navParams.data.retweeted_status ? navParams.data.retweeted_status : navParams.data;
+
+        if (navParams.data.retweeted_status) {
+            this.isRetweet = true;
+            this.parentTweet = navParams.data;
+        }
+
+        console.log(this.selectedTweet.retweeted_status)
     }
 
 }
