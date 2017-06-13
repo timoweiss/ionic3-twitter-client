201dc2aeb5d91e95604e6492600756f5f02231e3
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-13 09:46:33 +0200

    first draft tweet detail, use inline template

--- src/pages/tweet-detail/tweet-detail.ts
+++ src/pages/tweet-detail/tweet-detail.ts
@@ -9,7 +9,50 @@ import { IStatus } from '../../services/twitter.service';
 @IonicPage()
 @Component({
     selector: 'tweet-detail',
-    templateUrl: 'tweet-detail.html'
+    // templateUrl: 'tweet-detail.html',
+    template: `
+        <ion-header>
+            <ion-navbar>
+                <ion-title>Tweet detail</ion-title>
+            </ion-navbar>
+
+        </ion-header>
+        <ion-content>
+            <ion-card>
+            <ion-item>
+                <ion-avatar item-start>
+                <img src="{{selectedTweet.user.profile_image_url_https}}">
+                </ion-avatar>
+                <h2>{{selectedTweet.user.name}}</h2>
+                <p>@{{selectedTweet.user.screen_name}}</p>
+            </ion-item>
+
+                <ion-card-content>
+                    {{selectedTweet.text}}
+                </ion-card-content>
+
+                <ion-row>
+                    <ion-col>
+                    <button ion-button icon-left clear small>
+                        <ion-icon name="star"></ion-icon>
+                        <div>12 Likes</div>
+                    </button>
+                    </ion-col>
+                    <ion-col>
+                    <button ion-button icon-left clear small>
+                        <ion-icon name="repeat"></ion-icon>
+                        <div>4 Comments</div>
+                    </button>
+                    </ion-col>
+                    <ion-col center text-center>
+                    <ion-note>
+                        {{selectedTweet.created_at | date}}
+                    </ion-note>
+                    </ion-col>
+                </ion-row>
+            </ion-card>
+        </ion-content>
+    `
 })
 export class TweetDetailPage {
 
