eec43af2a5d9e9ec2a5a36fae2881749a7de80f1
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-14 12:56:39 +0200

    add sortpipe to declarations

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -4,9 +4,14 @@ import { HomePage } from './home';
 import { IonicPageModule } from 'ionic-angular';
 import { TweetList } from '../../components/TweetList/TweetList.component';
 import { SearchPipe } from '../../pipes/search/search';
+
+import { SorttweetsPipe } from '../../pipes/sorttweets/sorttweets';
+
+
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
+
 @NgModule({
-    declarations: [HomePage, TweetList, SearchPipe],
+    declarations: [HomePage, TweetList, SearchPipe, SorttweetsPipe],
     imports: [IonicPageModule.forChild(HomePage)],
     providers: [SpeechRecognition]
 })
