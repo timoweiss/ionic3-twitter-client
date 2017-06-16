0e97119527e12d80e2f95648208cbce81ae3046c
Timo Weiß <t.weiss@mwaysolutions.com>
2017-06-16 11:07:11 +0200

    include inappopener componenet#

--- src/pages/tweet-detail/tweet-detail.module.ts
+++ src/pages/tweet-detail/tweet-detail.module.ts
@@ -4,13 +4,16 @@ import { TweetDetailPage } from './tweet-detail';
 import { IonicPageModule } from 'ionic-angular';
 
 import { LinkyModule } from 'angular-linky';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { TweetDetail } from '../../components/TweetDetail/TweetDetail.component'
+import { InappopenerComponent } from '../../components/inappopener/inappopener';
 
 @NgModule({
-    declarations: [TweetDetailPage, TweetDetail],
+    declarations: [TweetDetailPage, TweetDetail, InappopenerComponent],
     imports: [
         IonicPageModule.forChild(TweetDetailPage),
         LinkyModule
     ],
+    providers: [InAppBrowser]
 })
 export class TweetDetailPageModule { }
